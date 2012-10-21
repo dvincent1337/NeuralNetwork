5ac031f2572ca1b5737b1ee979acff4346373269
David Vincent <dvincent1337@gmail.com>
2012-10-21 18:37:38 -0500

    moved files around

---
+++ src/neuralNet/CostFunction.java
@@ -0,0 +1,17 @@
+package neuralNet;
+
+import neuralNet.Tuple;
+
+import org.jblas.DoubleMatrix;
+
+
+public interface CostFunction 
+{
+
+	  /**
+	   *  Returns a Tuple with first element the Cost, and the second element the Gradients of given input matrix
+	   */
+	  public Tuple<Double, DoubleMatrix> evaluateCost(DoubleMatrix input);
+
+}
+
